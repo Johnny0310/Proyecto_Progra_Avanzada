1ea487b1ffeee93cdc4a2a83368484fcaa092a58
Rooster_GitHub <johnnygallo10@gmail.com>
2024-07-17 21:31:19 -0600

    Initial Commit

---
+++ Proyecto_Progra_Avanzada/Proyecto_Progra_Avanzada/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_Progra_Avanzada.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
