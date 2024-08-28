8fc815503622261740feb1a7514a3e6e290d8dde
Wayner Jiménez <waynerjive@gmail.com>
2024-08-27 23:31:56 -0600

    Roles MVC

--- Proyecto_Progra_Avanzada/Data/ApplicationDbContext.cs
+++ Proyecto_Progra_Avanzada/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Progra_Avanzada.Models;
+using System.Reflection.Emit;
 
 namespace Proyecto_Progra_Avanzada.Data
 {
@@ -15,12 +16,16 @@ namespace Proyecto_Progra_Avanzada.Data
         public DbSet<MensajeChat> MensajesChat { get; set; }
         public DbSet<ParticipanteChat> ParticipantesChat { get; set; }
 
+        public DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public DbSet<AspNetUsers> AspNetUsers { get; set; }
         public DbSet<Pokedex> Pokedex { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            builder.Entity<AspNetRoles>()
+            .ToTable("AspNetRoles");
 
         }
     }
--- Proyecto_Progra_Avanzada/Models/AspNetRoles.cs
+++ Proyecto_Progra_Avanzada/Models/AspNetRoles.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Progra_Avanzada.Models
 {
-    public class AspNetRoles
+    public class AspNetRoles : IdentityRole
     {
-        [Key]
+        
         public string RoleId { get; set; }
         public string Name { get; set; }
         public string NormalizedName { get; set; }
+        public string Descripcion {  get; set; }
         public string ConcurrencyStamp { get; set; }
     }
+    
 }
